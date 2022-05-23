03b3a50d0835c9e2f44021900ae2d440986ebc30
ruxipop <101018474+ruxipop@users.noreply.github.com>
2022-05-23 09:48:55 +0300

    Delete Software Design/Project/Server directory

--- Software Design/Project/Server/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import com.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Server s = new Server(5001);
-        s.run();
-    }
-}
--- Software Design/Project/Server/src/main/java/decorator/Notifier.java
+++
@@ -1,5 +0,0 @@
-package decorator;
-
-public interface Notifier {
-    void send(String message);
-}
